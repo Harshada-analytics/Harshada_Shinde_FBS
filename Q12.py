0226979eff66fe33226917d003bd6c34e2ce6518
Harshada_Shinde <harshadashinde0406@gmail.com>
2026-07-15 10:09:54 +0530

    Delete Q12.py

--- Q12.py
+++
@@ -1,7 +0,0 @@
-#Q12. Find the volume of sphere.
-
-radius = float(input("Enter the radius of the sphere :"))
-
-volume = (4/3) * 3.14 * radius * radius * radius
-
-print("Volume of the sphere =",volume)
